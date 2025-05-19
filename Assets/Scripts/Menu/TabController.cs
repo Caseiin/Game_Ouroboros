e810c357568c5849a9fe65fed29f459384c0d7f4
Caseiin <leruopono@gmail.com>
2025-05-19 21:55:07 +0200

    Edited the tab menu

--- Assets/Scripts/Menu/TabController.cs
+++ Assets/Scripts/Menu/TabController.cs
@@ -14,13 +14,30 @@ public class Tab : MonoBehaviour
 
     public void ActiveTabs(int tabNo)
     {
-        for (int i =0; i< pages.Length; i++)
+        for (int i = 0; i < pages.Length; i++)
         {
             pages[i].SetActive(false);
-            tabImages[i].color = Color.red;
+            tabImages[i].color = Color.gray;
         } //sets all tabs and pages to be unselected
 
         pages[tabNo].SetActive(true);
-        tabImages[tabNo].color = Color.white;
+        //tabImages[tabNo].color = Color.white;
+
+        switch (tabNo)
+        {
+            case 0:
+                tabImages[tabNo].color = Color.red;
+                break;
+            case 1:
+                tabImages[tabNo].color = new Color(92f, 64f, 51f);
+                break;
+            case 2:
+                tabImages[tabNo].color = Color.yellow;
+                break;
+            case 3:
+                tabImages[tabNo].color = Color.green;
+                break;
+
+        }
     }
 }
