2625b39074e3e1a6a99928cdef4060153028e4a7
Caseiin <leruopono@gmail.com>
2025-06-14 19:53:53 +0200

    comment Initiializer script

--- Assets/Scripts/Scenes/Initializer.cs
+++ Assets/Scripts/Scenes/Initializer.cs
@@ -2,12 +2,12 @@ using UnityEngine;
 
 public class Initializer : MonoBehaviour
 {
-    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    // [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
 
-    public static void Execute()
-    {
-        Debug.Log("Loaded by the persis objects from the Initializer script in the scene folder ");
-        Object.DontDestroyOnLoad(Object.Instantiate(Resources.Load("PERSISTOBJECTS")));
+    // public static void Execute()
+    // {
+    //     Debug.Log("Loaded by the persis objects from the Initializer script in the scene folder ");
+    //     Object.DontDestroyOnLoad(Object.Instantiate(Resources.Load("PERSISTOBJECTS")));
 
-    }
+    // }
 }
