907aa2cbf25eb0955e8d673156c3b7a3f7434e46
Caseiin <leruopono@gmail.com>
2025-06-15 22:20:02 +0200

    melee enemy improved
    the melee enemy improved so now it chases the player after going into the combat state

--- Assets/Scripts/Enemy/E_FSM/e_MeleeState.cs
+++ Assets/Scripts/Enemy/E_FSM/e_MeleeState.cs
@@ -11,11 +11,18 @@ public class e_MeleeState : e_CombatState
     public override void UpdateState(EnemyStateManager enemyState)
     {
         //Transition checks
-        if (!enemyState.enemy.PlayerDetected())
+        if (enemyState.enemy.PlayerDetected() && !enemyState.enemy.WithinCombatRange())
         {
-            enemyState.SwitchState(enemyState.PatrolState);
-            return; //important return
+            enemyState.SwitchState(enemyState.ChaseState);
+            return;
         }
+
+
+        if (!enemyState.enemy.PlayerDetected())
+            {
+                enemyState.SwitchState(enemyState.PatrolState);
+                return; //important return
+            }
 
         enemyState.enemy.MeleeAttack();
     }
